bbf065f5cdb20f247417d3b4aa416e81d753459c
NavyaNakka <nakkanavya231@gmail.com>
2025-11-19 15:01:32 +0530

    Update userRoutes.js

--- Exam Portal/server/routes/userRoutes.js
+++ Exam Portal/server/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dashboardController from "../controllers/userDashboard.js";
+import userExamsController from "../controllers/userExams.js";
 
 const router = express.Router();
 
 router.use("/", dashboardController);
+router.use("/", userExamsController);
 
 export default router;
