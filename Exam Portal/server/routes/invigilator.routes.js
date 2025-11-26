167086746bff749270f15f980e7ba157f9dec3e9
leela-prasanna-admin <leelaprasannamuthyala@gmail.com>
2025-11-26 12:53:45 +0530

    invigilator_dashboardTask12-compl

--- Exam Portal/server/routes/invigilator.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getInvigilatorDashboard } from "../controllers/invigilator_dashboard.js";
-
-const router = express.Router();
-
-router.get("/dashboard", getInvigilatorDashboard);
-
-export default router;
